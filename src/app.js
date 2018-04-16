ff974441a198e749f32044ffc2416ded88adb374
Teerapong Singthong <st.teerapong@gmail.com>
2018-04-16 16:45:30 +0700

    Add patch method and add redis.conf

--- src/app.js
+++ src/app.js
@@ -1,61 +1,145 @@
+/**
+ * @author  Teerapong, Singthong
+ * @description
+ *  Dear All friend,
+ * 
+ *  Please note that this example is simply to show how we implment caching layer
+ *  with Redis and NodeJS, so do not use all example in prodution.
+ * 
+ *  Please refactoring first !!!!
+ */
+
 const express = require("express");
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const todos = require('./models/todos');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const todos = require("./models/todos");
+const { promisify } = require("util");
+
+const redisClient = require("redis").createClient;
+const redis = redisClient(6379, "localhost");
+const getAsync = promisify(redis.get).bind(redis);
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// mongoose.connect('mongodb://my-mongodb/todo');
-mongoose.connect('mongodb://localhost:27017/todos');
+mongoose.connect("mongodb://my-mongodb/todo");
+// mongoose.connect("mongodb://localhost:27017/todos");
 
 const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function() {
-//   // we're connected!
-// });
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected MongoDB");
+});
 
-app.get('/todos', async (req, res) => {
-  const result = await todos.find({});
+redis.on("error", err => {
+  console.error("Redis Error " + err);
+});
+
+app.get("/todos", async (req, res) => {
+  try {
+    const result = await todos.find({});
+    res.status(200).json({
+      success: true,
+      data: result
+    });
+  } catch (e) {
+    console.error("Unable to get todos", e);
+  }
+});
+
+// get todo by title
+app.get("/todo/:title", async (req, res) => {
+  try {
+    const title = req.params.title;
+
+    if (!title) {
+      res.status(400).json({
+        success: false,
+        message: "Invalid param"
+      });
+    }
+
+    // find from cache
+    let getTitleDataFromCache = await getAsync(title);
+
+    if (!getTitleDataFromCache) {
+      let result = await todos.findOne({ title: title });
+
+      // set to cache
+      await redis.set(title, JSON.stringify(result));
+
+      res.status(200).json({
+        success: true,
+        source: "mongodb",
+        data: result
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      source: "redis",
+      data: JSON.parse(getTitleDataFromCache)
+    });
+  } catch (e) {
+    console.error("Unable to get todo by title", e);
+  }
+});
+
+// create new todo
+app.post("/todo", async (req, res) => {
+  try {
+    if (!req.body) {
+      res.status(400).json({
+        success: false,
+        message: "Please send the todo body"
+      });
+    }
+
+    let todo = new todos(req.body);
+    await todo.save();
+    res.status(200).json({
+      success: true,
+      data: todo
+    });
+  } catch (e) {
+    console.error("Unable to add new todo", e);
+  }
+});
+
+// update todo status by title
+app.patch("/todo/:title", async (req, res) => {
+  const title = req.params.title;
+  const isDone = req.body.isDone;
+
+  if (!title) {
+    res.status(400).json({
+      success: false,
+      message: "Please send the todo title"
+    });
+  }
+
+  // update mongodb
+  let result = await todos.findOneAndUpdate(
+    {
+      title: title
+    },
+    {
+      $set: {
+        isDone: isDone
+      }
+    },
+    { new: true }
+  );
+
+  // update cache
+  await redis.set(title, JSON.stringify(result));
+
   res.status(200).json({
     success: true,
     data: result
   });
 });
-
-app.get('/todo/:title', async (req, res) => {
-  if(!req.params.title) {
-    res.status(400).json({
-      success: false,
-      message: "Invalid param"
-    });
-  }
-
-  const result = await todos.findOne({ title: req.params.title });
-  res.status(200).json({
-    success: true,
-    data: result
-  });
-});
-
-app.post('/todo', async (req, res) => {
-  if (!req.body) {
-    res.status(400).json({
-      success: false,
-      message: "Please send the todo body"
-    });
-  }
-
-  let todo = new todos(req.body);
-  await todo.save();
-  res.status(200).json({
-    success: true,
-    data: todo
-  });
-});
-
-
 
 app.listen(8080, () => {
   console.log("Listening on port 8080");
